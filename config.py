a332e90a07a308a84c65aaa5b7ca8f42132a3cb3
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2025-06-21 21:05:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,8 +54,8 @@ class Config(object):
     FREE_UPLOAD_LIMIT = 6442450944 # calculation 6*1024*1024*1024=results
 
     # premium mode feature ✅
-    UPLOAD_LIMIT_MODE = True 
-    PREMIUM_MODE = True 
+    UPLOAD_LIMIT_MODE = False
+    PREMIUM_MODE = False
     
     #force subs
     try:
