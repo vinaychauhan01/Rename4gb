e5b3c2a7099d23c466fc7c68a2cdf3f98d47eadf
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-06-26 17:14:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ License Link : https://github.com/DigitalBotz/Digital-Rename-Bot/blob/main/LICEN
 import aiohttp, asyncio, warnings, pytz, datetime
 import logging
 import logging.config
-import glob, sys, importlib
+import glob, sys, importlib, pyromod
 from pathlib import Path
 
 # pyrogram imports
