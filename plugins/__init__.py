5504f79bb0ad2231cbc0c5ebb2e2a1afad6a0dc6
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-07-31 21:41:55 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,30 +1,5 @@
-#  Telegram MTProto API Client Library for Pyrogram
-#  Copyright (C) 2017-present DigitalBotz <https://github.com/DigitalBotz>
-#  I am a telegram bot, I created it using pyrogram library. https://github.com/pyrogram
-"""
-Apache License 2.0
-Copyright (c) 2022 @Digital_Botz
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Telegram Link : https://t.me/Digital_Botz 
-Repo Link : https://github.com/DigitalBotz/Digital-Rename-Bot
-License Link : https://github.com/DigitalBotz/Digital-Rename-Bot/blob/main/LICENSE
-"""
-from pyromod import ListenerTimeout
+from pyromod.exceptions import ListenerTimeout
 
 __name__ = "Digital-Rename-Bot"
 __version__ = "3.0.8"
