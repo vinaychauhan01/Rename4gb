0a8f0af510e40eed4336025774733f82fbe973a4
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-08-01 16:28:51 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -2,7 +2,7 @@
 
 # pyrogram imports
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.enums import MessageMediaType
 from pyrogram.errors import FloodWait
 from pyrogram.file_id import FileId
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
